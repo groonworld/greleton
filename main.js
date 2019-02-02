5e8f7559b7ef352a964737418dd073089d2d0a79
Phil Groonworld <46387880+groonworld@users.noreply.github.com>
2019-02-02 22:35:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,36 +2,64 @@
 /*global define, $, brackets */
 
 
-/** Simple extension that adds "File > Insert HTML Skeleton" and "File > Insert CSS Skeleton" menu items. 
-Inserts  at cursor pos. */
+/* Simple extension which adds "File > Insert HTML Skeleton" and "File > Insert CSS Skeleton" menu items. Haven't yet figured out how to insert text on multiple lines. */
+
+/*********************************************************************************
+ *                                                                               *
+ * TO DO: Figure out how to make the inserted strings span multiple lines.       *   
+ * This extension is quite useless until that works.                             *
+ *                                                                               *
+ * FUTURE: Set document syntax highlighting to CSS or HTML, accordingly.         *
+ * FUTURE: Add settings to allow users to individualize their template-skeletons *
+ *                                                                               *
+ *********************************************************************************/
+
+
 define(function (require, exports, module) {
     "use strict";
 
     let CommandManager = brackets.getModule("command/CommandManager"),
         EditorManager  = brackets.getModule("editor/EditorManager"),
         Menus          = brackets.getModule("command/Menus"),
-        stringHTML     = "This is going to be the HTML skeleton.",  // TODO: Provide actual HTML Skeleton
-        stringCSS      = "This is going to be the CSS skeleton.";   // TODO: Provide actual CSS Skeleton
+        stringHTML     = ["<!DOCTYPE html>", 
+                          "<html lang=\"de\">"], //currently trying to get this to span multiple lines.
+        stringCSS      = "This is where the CSS skeleton is going to be.";   // TODO: Provide actual CSS Skeleton
         
     
-    // Function to run when the menu item is clicked
-    function insertSkeleton() {
+    // Functions to run when the menu item is clicked
+    function insertHTML() {
+        console.log("1. insertHMTL() has been called.")
         let editor = EditorManager.getFocusedEditor();
         if (editor) {
-            let insertionPos = editor.getCursorPos();
-            editor.document.replaceRange(stringHTML, insertionPos);
+            let insnPos = {line: 0, ch: 0};
+            
+            console.log("2. " + insPos);
+            console.log("3. " + stringHTML);
+            for (var i = 0; i < stringHTML.length; i++) {
+                console.log("4. i: " + i);
+                insPos.line = i + 5;
+                console.log("5. " + stringHTML[i] + ", Line:" + insPos.line);
+                editor.document.replaceRange(stringHTML[i], insPos, insPos);
+            };
         }
     }
-    let insertHTML = insertSkeleton(stringHTML);
-    let insertCSS  = insertSkeleton(stringCSS);
+    
+    function insertCSS() {
+        let editor = EditorManager.getFocusedEditor();
+        if (editor) {
+            let insPos = {line: 0, ch: 0};
+            editor.document.replaceRange(stringCSS, insPos);
+        }
+    }
+  
     
     // First, register a command - a UI-less object associating an id to a handler.
     // Use package-style naming to avoid collisions
     const COMMAND_ID_HTML = "greleton.insertHTML";
     const COMMAND_ID_CSS  = "greleton.insertCSS";     
     
-    CommandManager.register("Insert HTML skeleton", COMMAND_ID_HTML, insertSkeleton); 
-    CommandManager.register("Insert CSS skeleton" , COMMAND_ID_CSS,  insertSkeleton); 
+    CommandManager.register("Insert HTML skeleton", COMMAND_ID_HTML, insertHTML); 
+    CommandManager.register("Insert CSS skeleton" , COMMAND_ID_CSS,  insertCSS); 
 
     // Then create  menue items bound to the commands
     // The label of the menu items is the name we gave the command (see above)
@@ -43,11 +71,6 @@ define(function (require, exports, module) {
     //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-W");
     // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
 });
-
-
-
-// FUTURE: Set document highlighting to .css or .html, accordingly.
-// FUTURE: Add settings allowing for individualized template-skeletons
 
 
 
